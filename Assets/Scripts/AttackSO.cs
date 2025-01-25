4f7044308b04defeb10461e5c6e4cd7e0185af50
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2025-01-25 20:11:07 +0000

    feat: create attack scriptable obhecct

---
+++ Assets/Scripts/AttackSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AttackSO", menuName = "Scriptable Objects/AttackSO")]
+public class AttackSO : ScriptableObject
+{
+    public Vector3 orientation;
+    public float scale;
+}
