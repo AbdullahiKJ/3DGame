a013848280a05e09347b96f02055a221c705e8dc
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2025-03-07 16:38:45 +0000

    feat(teleport): implement initial teleport prototype

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -1,58 +1,67 @@
 using Unity.Cinemachine;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Teleport : MonoBehaviour
 {
-    [SerializeField] float teleportSpeed = 5f;
-    [SerializeField] LayerMask teleportLayer;
-    [SerializeField] Animator cameraAnimator;
     [SerializeField] CinemachineCamera freeLookCam;
-    [SerializeField] CinemachineCamera aimCam;
+    [SerializeField] GameObject aimIndicator;
+    [SerializeField] Vector2 defaultAxisGain;
+    [SerializeField] float indicatorSpeed;
 
     public Camera mainCamera;
+    CinemachineInputAxisController axisController;
     Movement movement;
+    Vector2 moveInput;
+    CharacterController controller;
     bool isTeleporting = false;
     bool isAiming = false;
-    float maxDistance = 100f;
-    // DEBUG
-    RaycastHit hit;
-    bool hitFound = false;
+    bool isSettingAimPosition = true;
+    [SerializeField] float maxDistance = 100f;
+    [SerializeField] float timeLimit = 5f;
+    float timer = 0f;
 
     void Awake()
     {
         movement = GetComponent<Movement>();
+        controller = GetComponent<CharacterController>();
+        aimIndicator.SetActive(false);
+        axisController = freeLookCam.GetComponent<CinemachineInputAxisController>();
+
+        SetAxisControllerGain(defaultAxisGain);
     }
 
     void Update()
     {
+        // Move the aim indicator when aiming
+        if (isAiming)
+        {
+            Vector3 movement = Quaternion.Euler(0f, mainCamera.transform.eulerAngles.y, 0f) * new Vector3(moveInput.x, 0, moveInput.y);
+            Vector3 newPosition = aimIndicator.transform.position + movement * indicatorSpeed;
 
-    }
+            float distanceFromPlayer = (newPosition - transform.position).magnitude;
+            if (distanceFromPlayer < maxDistance)
+                aimIndicator.transform.position = newPosition;
+        }
 
-    void LateUpdate()
-    {
-
-    }
-
-    // DEBUG
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        if (hitFound)
-            Gizmos.DrawSphere(transform.position, 0.25f);
+        // Ability timer
+        if (isTeleporting && timer < timeLimit)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            isTeleporting = false;
+            timer = 0f;
+        }
     }
 
     void OnTeleport(InputValue value)
     {
-        if (value.isPressed && !movement.getIsRolling())
+        if (value.isPressed && !movement.getIsRolling() && !isTeleporting)
         {
             isTeleporting = true;
             StartTeleport();
-        }
-        else
-        {
-            StopTeleport();
         }
     }
 
@@ -60,24 +69,30 @@ public class Teleport : MonoBehaviour
     {
         if (value.isPressed && !isTeleporting && !movement.getIsRolling())
         {
-            cameraAnimator.Play("AimCam");
             isAiming = true;
 
-            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, maxDistance, teleportLayer))
+            if (isSettingAimPosition)
             {
-                hitFound = true;
+                SetAimPosition();
             }
-
-            // Set the free look camera orientation and rotation to that of the lock on camera
-            freeLookCam.ForceCameraPosition(aimCam.transform.position, aimCam.transform.rotation);
         }
         else
         {
-            cameraAnimator.Play("FreeLookCam");
             isAiming = false;
+            isSettingAimPosition = true;
+            aimIndicator.SetActive(false);
 
-            // Set the free look camera orientation and rotation to that of the lock on camera
-            aimCam.ForceCameraPosition(freeLookCam.transform.position, freeLookCam.transform.rotation);
+            // Edit axis controller gain
+            SetAxisControllerGain(defaultAxisGain);
+        }
+    }
+
+
+    void OnLook(InputValue value)
+    {
+        if (isAiming)
+        {
+            moveInput = value.Get<Vector2>();
         }
     }
 
@@ -88,18 +103,45 @@ public class Teleport : MonoBehaviour
 
     void StartTeleport()
     {
-        if (hitFound == true)
-        {
-            // Use coroutine to wait a certain amount of time
+        // todo create a coroutine to wait a certain amount of time
 
-            // Teleport to the raycast hit
-            transform.position = hit.point;
+        // todo create and play an animation or activate a shader
+
+        // Teleport to the raycast hit
+        Vector3 newPosition = aimIndicator.transform.position + new Vector3(0f, 2f, 0f) - transform.position;
+        controller.Move(newPosition);
+
+        // Deactivate the indicator
+        aimIndicator.SetActive(false);
+    }
+
+    void SetAimPosition()
+    {
+        // Edit axis controller gain
+        SetAxisControllerGain(new Vector2(100f, -0.1f));
+
+        // Set the indicator game object to active
+        aimIndicator.SetActive(true);
+        isSettingAimPosition = false;
+
+        // Spawn the aim indicator
+        Vector3 camForward = mainCamera.transform.forward;
+
+        // Use only the x and y axes of the camera's forward rotation
+        Vector3 spawnPoint = transform.position + new Vector3(camForward.x, 0f, camForward.z) * 5f;
+        aimIndicator.transform.position = spawnPoint;
+        return;
+    }
+
+    // Set the input axis gain for the free look camera
+    void SetAxisControllerGain(Vector2 gain)
+    {
+        foreach (var c in axisController.Controllers)
+        {
+            if (c.Name == "Look Orbit X")
+                c.Input.Gain = gain.x;
+            else
+                c.Input.Gain = gain.y;
         }
     }
-
-    void StopTeleport()
-    {
-        isTeleporting = false;
-    }
-
 }
