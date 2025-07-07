91028368eae92357c4d99b5770d2b1992d7e6f78
AbdullahiKJ <154251356+AbdullahiKJ@users.noreply.github.com>
2025-07-07 19:02:44 +0100

    feat: make damage scriptable objects

---
+++ Assets/Scripts/Scriptable Objects/Damage/DamageSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DamageSO", menuName = "Scriptable Objects/Damage")]
+public class DamageSO : ScriptableObject
+{
+    public float baseDamage = 10f; // Amount of damage dealt
+    // TODO: Add elemental damage and status effects
+    public bool isFireDamage = false; // Whether the damage is fire-based
+    public bool isIceDamage = false; // Whether the damage is ice-based
+    public bool isPoisonDamage = false; // Whether the damage is poison-based
+    public float multiplier = 1f; // Multiplier for the damage, can be used to increase or decrease damage based on conditions
+
+}
